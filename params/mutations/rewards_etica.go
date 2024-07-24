e3d8fe3abca84beac89f96bb5584bae4b5ae3153
Kevin Wad <noemail@eticaprotocol.org>
2024-07-24 16:54:50 +0200

    Updates rewards_etica.go according to latest etc-core-geth release new vars types

--- params/mutations/rewards_etica.go
+++ params/mutations/rewards_etica.go
@@ -16,28 +16,26 @@
 package mutations
 
 import (
-	"math/big"
-
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params/types/ctypes"
 	"github.com/ethereum/go-ethereum/params/vars"
+	"github.com/holiman/uint256"
 )
 
-func etip1017BlockReward(config ctypes.ChainConfigurator, header *types.Header, uncles []*types.Header) (*big.Int, []*big.Int) {
+func etip1017BlockReward(config ctypes.ChainConfigurator, header *types.Header, uncles []*types.Header) (*uint256.Int, []*uint256.Int) {
 	blockReward := vars.ETIP1017BlockReward // EGAZ tail emission, 2 EGAZ per block
 
-	
 	// Accumulate the rewards for the miner and any included uncles
-	uncleRewards := make([]*big.Int, len(uncles))
-	reward := new(big.Int).Set(blockReward)
-	r := new(big.Int)
+	uncleRewards := make([]*uint256.Int, len(uncles))
+	reward := new(uint256.Int).Set(blockReward)
+	r := new(uint256.Int)
 	for i, uncle := range uncles {
-		r.Add(uncle.Number, big8)
-		r.Sub(r, header.Number)
+		r.Add(uint256.MustFromBig(uncle.Number), big8)
+		r.Sub(r, uint256.MustFromBig(header.Number))
 		r.Mul(r, blockReward)
 		r.Div(r, big8)
 
-		ur := new(big.Int).Set(r)
+		ur := new(uint256.Int).Set(r)
 		uncleRewards[i] = ur
 
 		r.Div(blockReward, big32)
