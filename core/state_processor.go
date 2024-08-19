eb80a0470079b6cd59ee2337bafc407abdf72428
Kevin Wad <noemail@eticaprotocol.org>
2024-08-19 15:05:09 +0200

    Starts implementation of Crucible option on VeirfyEticaTransaction()

--- core/state_processor.go
+++ core/state_processor.go
@@ -222,7 +222,7 @@ func ApplyTransaction(config ctypes.ChainConfigurator, bc ChainContext, author *
 		configEticaChainId := config.GetChainID()
 		fmt.Printf("5µ5µµ5µ5µ5µ5µ --- >configEticaChainId (hex): %x\n", configEticaChainId)
 		const EticaChainId = 61803
-		const CrucibleChainId = 61888
+		const CrucibleChainId = 818889
 		// Convert *big.Int to uint64
 		configEticaChainIdUint64 := configEticaChainId.Uint64()
 		fmt.Printf("5µ5µµ5µ5µ5µ5µ --- > configEticaChainIdUint64 (decimal): %d\n", configEticaChainIdUint64)
