1ccbe955bf0cae6a9cc8b861b092dd7000f8bb35
Kevin Wad <noemail@eticaprotocol.org>
2024-08-20 13:55:48 +0200

    Updates Crucible config for test update smart contract bytecode

--- params/config_crucible.go
+++ params/config_crucible.go
@@ -103,7 +103,7 @@ var (
 
 		EticaSmartContractv2: big.NewInt(703_000), // Etica smart contract (Meticulous, Etica Hardfork 1)
 
-		EticaSmartContractv3: big.NewInt(179_902), // Etica smart contract (Guardian, Etica Hardfork 2) Warning: dont forget set to RandomX hardfork height
+		EticaSmartContractv3: big.NewInt(180_005), // Etica smart contract (Guardian, Etica Hardfork 2) Warning: dont forget set to RandomX hardfork height
 		EticaRandomX:         big.NewInt(173_460), // Etica smart contract (Guardian, Etica Hardfork 2) [EticaRandomX needs EticaSmartContractv3] Warning: dont forget set to RandomX hardfork height
 
 	}
--- params/vars/etica_upgrades.go
+++ params/vars/etica_upgrades.go
@@ -48,7 +48,7 @@ var Eticav2ForkExtraRange = big.NewInt(10)
 
 // --------- main smart contract loads bytecode from following contract ----------- //
 var EticaSmartContractAddressv3 = common.HexToAddress("0x64cB3Bc8cF8324432838B5c58519F782482C9861") // Etica v3, Guardian Hardfork
-var CrucibleSmartContractAddressv3 = common.HexToAddress("0xCF9c216399ec036F565240a1DC93cf4251538738")
+var CrucibleSmartContractAddressv3 = common.HexToAddress("0xA9aEe690Cb5cfC2AEcede8a0Dc259c4697b66a81")
 
 // Eticav3ForkBlockExtra is the block header extra-data field to set for the Eticav3 fork
 // point and a number of consecutive blocks to allow fast/light syncers to correctly
